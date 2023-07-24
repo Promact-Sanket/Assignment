65ec45c4bbe777f851f79ac858289f05841aed7e
Sanket <Sanket@promactinfo.com>
2023-07-24 16:29:42 +0530

    Adds employee management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUpdateEmployeeComponent } from './add-update-employee/add-update-employee.component';
+import { ListEmployeeComponent } from './list-employee/list-employee.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'list',   
+    component:ListEmployeeComponent ,    
+  },
+  {
+    path:'create',
+    component:AddUpdateEmployeeComponent
+  },
+  {
+    path:'edit/:id',
+    component:AddUpdateEmployeeComponent
+  },
+  {
+    path: '',   
+    component:ListEmployeeComponent ,    
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
